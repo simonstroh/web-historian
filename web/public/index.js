dfb2b6b40ab905f6b1fbc3ab5fb36bacf9f698ac
Simon Stroh <simon_stroh@outlook.com>
2018-05-16 12:52:00 -0700

    Switching to branch test

---
+++ web/public/index.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<ReactInput />, document.getElementById('app'))
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,44 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs')
+var helpers = require('./http-helpers')
+
+var list = []
+
+var post = function(req) {
+  var body = ''
+  req.on('data', (chunk) => body += chunk)
+  req.on('end', () => list.push(JSON.parse(body)))
+}
+
+var external = {
+  '/styles.css': function(res) {
+    fs.readFile('./public/styles.css', function(err, data) {
+      if (err) console.log(err)
+      res.end(data)
+    })
+  },
+  '/': function(res) {
+    fs.readFile('./public/index.html', function(err, data) {
+      if (err) console.log(err);
+      res.end(data)
+    })
+  }
+}
+
+var actions = {
+  'GET': function(req, res) {
+    external[req.url](res)
+  },
+  'POST': function(req, res) {
+    post(req)
+  }
+}
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log("Serving request type " + req.method + " at url " + req.url)
+  var action = actions[req.method]
+  action(req, res)
+  // res.end(archive.paths.list);
 };
